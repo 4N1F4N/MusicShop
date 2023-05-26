fbceffee0297b1702d6fbf1d8c04c4540e15a279
4N1F4N <6t3f4n@gmail.com>
2023-05-26 16:44:02 +0300

    Add search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,8 +9,23 @@ class PagesController < ApplicationController
     @products = Product.all
   end
 
+  def search
+    @products = Product.all
+    @tag_id = 0
+
+    if params[:tag_id] && !(params[:tag_id] == "")
+      @tag_id = params[:tag_id].to_i
+      @products = Product.where(tag_id: params[:tag_id]).all
+    end
+  end
+
+  def about
+
+  end
+
   private
     def check_admin
       redirect_to "/" if !session[:profile_id] || !Profile.find(session[:profile_id]).admin?
     end
+
 end
